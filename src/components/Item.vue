84a383863881694910ac193adf11e2aff63b0435
dlindenbaum <dlindenbaum>
2018-08-12 09:46:02 -0400

    updated Item.vue to use lambda-tiler to support spaceNetS3

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -371,7 +371,7 @@ export default {
       }
 
       // TODO global config
-      return `https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url=${encodeURIComponent(
+      return `https://14ffxwyw5l.execute-api.us-east-1.amazonaws.com/production/tiles/{z}/{x}/{y}@2x?url=${encodeURIComponent(
         this.cog
       )}`;
       // return `http://localhost:8000/tiles/{z}/{x}/{y}@2x?url=${encodeURIComponent(
